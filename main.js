e9a7e46405933c4dc1100c0fabe2cdc7d8823be0
enes uludag <enes.uludag.1996@gmail.com>
2022-09-25 02:10:06 +0300

    .

--- main.js
+++ main.js
@@ -7,21 +7,25 @@ const table=process.argv[2];
 const filePath=process.argv[3];
 const fileExt = path.extname(filePath);
 let connection;
-let data;
+let datax;
 
-if(fileExt ==='.xlsx'){
-    //data = xlsx(filePath);
-    data = xlsx(filePath);
-    insert(table,data);
-} else if(fileExt ==='.csv'){
-    data = csv(filePath);
-    insert(table,data);
+async function main(){
+    if(fileExt ==='.xlsx'){
+        //data = xlsx(filePath);
+        datax = await xlsx(filePath);
+        //console.log( await datax);
+        //insert(table,data);
+    } else if(fileExt ==='.csv'){
+        datax = csv(filePath);
+        insert(table,datax);
+    }
+    else{
+        console.log('File format not supported');
+    }
+    
+    
 }
-else{
-    console.log('File format not supported');
-}
-
-
+main();
 async function csv(filePathx){
     const jsonArray=await csvtojsonV2().fromFile(filePathx);
     console.log(jsonArray);
@@ -31,7 +35,7 @@ async function csv(filePathx){
 async function  xlsx(path){
     var workbook = XLSX.readFile(path);
 var sheet_name_list = workbook.SheetNames;
-sheet_name_list.forEach(function(y) {
+ sheet_name_list.forEach(function(y) {
     var worksheet = workbook.Sheets[y];
     var headers = {};
     var data = [];
@@ -61,34 +65,36 @@ sheet_name_list.forEach(function(y) {
     data.shift();
     data.shift();
     console.log(data);
+    insert(table,data);
     return data;
 });
 }
 async function insert(table,values){
     try {
-    connection = await oracledb.getConnection({ user, password, connectionString });
+   // await fs.writeFileSync('1.json', eee);
+    //connection = await oracledb.getConnection({ user, password, connectionString });
     console.log("Successfully connected to Oracle Database");
     if (table === 'FIND_ENTITY') {
-        for (const value of values) {
+        for (let value of values) {
             // const query = 'insert into FIND_ENTITY (ENTITY_CODE,ENTITY_NAME,TAX_NO,COUNTRY_ID,CITY_ID,TOWN_ID,ADDRESS1) values (:1,:2,:3,:4,:5,:6,:7)';
             // const binds = [value.ENTITY_CODE, value.ENTITY_NAME, value.TAX_NO, value.COUNTRY_ID, value.CITY_ID, value.TOWN_ID, value.ADDRESS1];
             const sql = `INSERT INTO FIND_ENTITY VALUES (:ENTITY_CODE, :ENTITY_NAME, :TAX_NO, :COUNTRY_ID, :CITY_ID, :TOWN_ID, :ADDRESS1)`;
-            const result = await connection.execute(
+            const resulte = await connection.execute(
                 sql,
                 value,
                 { autoCommit: true }
                 );
-                console.log("GIDEN DATA",result);
+            console.log("GIDEN")
         }
 
-        console.log("DDD", values);
+        //console.log("DDD", values);
     } else{
-    for(const value of values) { 
+    /* for(const value of values) { 
         await connection.execute(
             `INSERT INTO ${table} (po_document) VALUES (:bv)`,
             { bv: {val: value, type: oracledb.DB_TYPE_JSON} }
         );
-    }
+    } */
 }
     if (connection) {
         try {
